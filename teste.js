b89d945c748d6babc8292b8f8dfc660f84b04a2c
lucasn782 <lucas.neves.carvalho@gmail.com>
2024-01-04 12:39:22 -0300

    Meu primeiro commit

---
+++ teste.js
@@ -0,0 +1,9 @@
+var numero = Math.floor(Math.random() * 100) + 1;
+
+console.log("Número gerado: " + numero);
+
+if (numero % 2 == 0) {
+    console.log(numero + " é par.");
+} else {
+    console.log(numero + " é ímpar.");
+}
